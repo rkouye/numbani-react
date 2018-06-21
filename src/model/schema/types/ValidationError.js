6df3fae88c20e80483925d3ee2bac595ef229e3a
I336025 <semako.kouye.houssou@sap.com>
2018-06-21 18:38:32 +0200

    Document ValidationError

--- src/model/schema/types/ValidationError.js
+++ src/model/schema/types/ValidationError.js
@@ -1,4 +1,10 @@
 class ValidationError {
+    /**
+     *Creates an instance of ValidationError.
+     * @param {String} message
+     * @param {*} info
+     * @memberof ValidationError
+     */
     constructor(message, info){
         this.message = message;
         this.info = info;
