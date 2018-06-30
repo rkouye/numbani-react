7084ad20ce3897dc59dcec318311e9040b5d3794
I336025 <semako.kouye.houssou@sap.com>
2018-06-30 19:43:37 +0200

    Basic in memory persistence provider

---
+++ src/model/persistence/provider/InMemory.js
@@ -0,0 +1,33 @@
+import Persistence from "../Persistence";
+
+class InMemoryPersistence extends Persistence {
+    constructor({ initialDB, logger }={}){
+        super();
+        this.logger = logger;
+        this.db = initialDB || {};
+    }
+
+    at(context){
+        this.db[context] = this.db[context] || {};
+        const clone = new InMemoryPersistence({initialDB : this.db[context], logger : this.logger});
+        return clone;
+    }
+
+    save(value, at){
+        this.db[at] = value;
+        this.logger.log(`${this} : Saved ${value} at ${at}`);
+        return Promise.resolve(at);
+    }
+
+    read(ref){
+        return Promise.resolve(this.db[ref]);
+    }
+
+    delete(ref){
+        delete this.db[ref];
+        this.logger.log(`${this} : Deleted value at ${ref}`);
+        return Promise.resolve();
+    }
+}
+
+export default InMemoryPersistence;
