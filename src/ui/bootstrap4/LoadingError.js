ab343afc34e0b15197199b5ac4656f993254e7b9
semako-kouye <romarickouye@gmail.com>
2018-07-21 17:21:59 +0200

    Allow LoadingError  and SavingError to take child

--- src/ui/bootstrap4/LoadingError.js
+++ src/ui/bootstrap4/LoadingError.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { injectEntityContext } from '../model/Entity';
 import { Alert } from 'reactstrap';
 
@@ -7,12 +8,26 @@ class LoadingErrorBase extends Component {
         const eC = this.props.entityContext;
         return (
             (!eC.isLoading && eC.loadingError)?
-            <Alert color="danger">{eC.loadingError.message || eC.loadingError.toString()}</Alert>
+            <Alert color="danger">{
+                (typeof this.props.children === 'function')?
+                this.props.children(eC.loadingError)
+                :
+                this.props.children || eC.loadingError.message || eC.loadingError.toString()
+            }</Alert>
             : null
         );
     }
 }
 
 const LoadingError = injectEntityContext(LoadingErrorBase, 'entityContext');
+
+LoadingError.propTypes = {
+    /**
+     * Either a React element to display or a function that take as parameter
+     * the error and returns the message to display or nothing, in this case
+     * the component will try do display the error anyway.
+     */
+    children : PropTypes.oneOfType(PropTypes.node, PropTypes.func)
+};
 
 export default LoadingError;
--- src/ui/bootstrap4/SavingError.js
+++ src/ui/bootstrap4/SavingError.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { injectEntityContext } from '../model/Entity';
 import { Alert } from 'reactstrap';
 
@@ -7,12 +8,27 @@ class SavingErrorBase extends Component {
         const eC = this.props.entityContext;
         return (
             (!eC.isSaving && eC.savingError)?
-            <Alert color="danger">{eC.savingError.message || eC.savingError.toString()}</Alert>
+            <Alert color="danger">
+            {
+                (typeof this.props.children === 'function')?
+                this.props.children(eC.savingError)
+                :
+                this.props.children || eC.savingError.message || eC.savingError.toString()
+            }</Alert>
             : null
         );
     }
 }
 
 const SavingError = injectEntityContext(SavingErrorBase, 'entityContext');
+
+SavingError.propTypes = {
+    /**
+     * Either a React element to display or a function that take as parameter
+     * the error and returns the message to display or nothing, in this case
+     * the component will try do display the error anyway.
+     */
+    children : PropTypes.oneOfType(PropTypes.node, PropTypes.func)
+};
 
 export default SavingError;
