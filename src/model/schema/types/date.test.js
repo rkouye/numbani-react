ddd04468f5da944de660db3eb3ba5ea96fded238
semako-kouye <romarickouye@gmail.com>
2018-07-28 12:40:53 +0200

    Disable date type

--- src/model/schema/types/date.test.js
+++ src/model/schema/types/date.test.js
@@ -1,7 +1,7 @@
 import types from './'
 import expect from 'expect';
 
-describe("Types", ()=>{
+xdescribe("Types", ()=>{
 
     describe("Date", ()=>{
 
--- src/model/schema/types/index.js
+++ src/model/schema/types/index.js
@@ -1,6 +1,6 @@
 import Array from './array';
 import Boolean from './boolean';
-import Date from "./date";
+//import Date from "./date";
 import Number from './number';
 import String from './string';
 export {Type} from './Type';
@@ -8,7 +8,7 @@ export {Type} from './Type';
 export default {
     Array,
     Boolean,
-    Date,
+//    Date,
     Number,
     String
 };
