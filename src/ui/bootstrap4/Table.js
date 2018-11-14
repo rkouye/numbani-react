9da6f59f2402fe3ab48fcf22d7ce45698506c264
semako-kouye <romarickouye@gmail.com>
2018-11-14 23:24:22 +0100

    Table show placeholder only after loading

--- src/ui/bootstrap4/Table.js
+++ src/ui/bootstrap4/Table.js
@@ -4,28 +4,29 @@ import { Table as BTable } from 'reactstrap';
 import Entity, { injectEntityContext } from '../model/Entity';
 import Attribute from './Attribute';
 
-const accessValue = (value, accessor) => typeof accessor==='function'?accessor(value):value[accessor];
+const accessValue = (value, accessor) => typeof accessor === 'function' ? accessor(value) : value[accessor];
 
 class BaseTable extends Component {
-     render() {
-        const {entityContext : ec, columns, index, placeholder, ...others} = this.props;
-            return (
-                (ec.loadedValues && ec.loadedValues.length > 0)?<BTable {...others} >
+    render() {
+        const { entityContext: ec, columns, index, placeholder, ...others } = this.props;
+        return (
+            ec.loadedValues ? (
+                (ec.loadedValues.length > 0) ? <BTable {...others} >
                     <thead><tr>
                         {
-                            columns.map((column,i) =><th key={i}>{column.header}</th>)
+                            columns.map((column, i) => <th key={i}>{column.header}</th>)
                         }
-                        </tr>
+                    </tr>
                     </thead>
                     <tbody>
                         {
-                            ec.loadedValues.map((value,i) =>
-                                <tr key={index?value[index]:i}>
+                            ec.loadedValues.map((value, i) =>
+                                <tr key={index ? value[index] : i}>
                                     <Entity repo={ec.repo} defaultValue={value}>
-                                        {columns.map((column,i) =>
+                                        {columns.map((column, i) =>
                                             <td key={i}>
                                                 {
-                                                    typeof column.cell === "function"?
+                                                    typeof column.cell === "function" ?
                                                         column.cell(value)
                                                         :
                                                         <Attribute name={column.cell} />
@@ -38,22 +39,23 @@ class BaseTable extends Component {
                         }
                     </tbody>
                 </BTable>
-                :
-                (placeholder || null)
-            );
+                    :
+                    (placeholder || null)
+            ) : null
+        );
     }
 }
 
 const Table = injectEntityContext(BaseTable);
 
 Table.propTypes = {
-    children : PropTypes.node,
-    columns : PropTypes.arrayOf(PropTypes.shape({
-        header : PropTypes.node.isRequired,
-        cell : PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired
+    children: PropTypes.node,
+    columns: PropTypes.arrayOf(PropTypes.shape({
+        header: PropTypes.node.isRequired,
+        cell: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired
     })).isRequired,
-    placeholder : PropTypes.node,
-    index : PropTypes.string,
+    placeholder: PropTypes.node,
+    index: PropTypes.string,
     size: PropTypes.string,
     bordered: PropTypes.bool,
     borderless: PropTypes.bool,
